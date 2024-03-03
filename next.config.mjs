8e5b0683ec8b5e453e404311f45cfe27e1e38f27
lucascn21 <lucascn21@gmail.com>
2024-03-02 21:35:43 -0300

    hacky config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,34 @@
 // @ts-check
 import withSerwistInit from "@serwist/next";
+import { watch } from "fs";
 
 const withSerwist = withSerwistInit({
   cacheOnFrontEndNav: true,
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
-  
 });
 
-/** @type {import("next").NextConfig} */
+/** @type {import("next/dist/next-server/server/config").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Configuración para observar cambios en archivos CSS en public/css y recargar automáticamente la página
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      const cssWatcher = watch("public/css", { recursive: true }, (eventType, filename) => {
+        if (filename && filename.endsWith(".css")) {
+          console.log("CSS file changed, reloading page...");
+          setTimeout(() => {
+            process.send("reloadPage");
+          }, 100);
+        }
+      });
+
+      process.on("exit", () => {
+        cssWatcher.close();
+      });
+    }
+    return config;
+  },
 };
 
 export default withSerwist(nextConfig);
